032d756306fd3e8d5d4daa97a3124ca093fd7ab7
jiyeonUning <yeon14735@gmail.com>
2024-08-27 05:33:06 +0900

    좌우회전,, 상하이동 구현

--- Assets/Write Code/BodyMove.cs
+++ Assets/Write Code/BodyMove.cs
@@ -4,44 +4,19 @@ using UnityEngine;
 
 public class BodyMove : MonoBehaviour
 {
-    [SerializeField] WingMove move;
-    [SerializeField] float wingRotateValue;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float rate;
+    [SerializeField] float rotateSpeed;
 
-    void Update()
+    private void Update()
     {
-        if (Input.GetButton("Jump"))
-        {
-            move.SpeedUp();
-        }
-        else
-        {
-            move.SpeedDown();
-        }
 
-        if (move.rotateSpeed > wingRotateValue)
-        { MoveUp(); }
-        else
-        { MoveDown(); }
-    }
+        float x = Input.GetAxis("Horizontal"); // A,D로 좌우 움직임
+        float z = Input.GetAxis("Vertical"); // W, S로 상하 움직임
 
-    public void MoveUp()
-    {
-        transform.Translate(Vector3.up * (move.rotateSpeed - wingRotateValue ) / 100 * Time.deltaTime);
-
-        if (transform.position.y > 5)
-        {
-            transform.position = new Vector3(transform.position.x, 5, transform.position.z);
-        }
-    }
-
-    public void MoveDown()
-    {
-        transform.Translate(Vector3.up * (move.rotateSpeed - wingRotateValue) / 100 * Time.deltaTime);
-
-        if (transform.position.y < 0)
-        {
-            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
-        }
-
+        // 전진과 후진 기능
+        transform.Translate(Vector3.right * z * moveSpeed * Time.deltaTime, Space.Self);
+        // 회전 기능
+        transform.Rotate(Vector3.up, x * rotateSpeed * Time.deltaTime, Space.World);
     }
 }
--- Assets/Write Code/WingMove.cs
+++ Assets/Write Code/WingMove.cs
@@ -4,33 +4,5 @@ using UnityEngine;
 
 public class WingMove : MonoBehaviour
 {
-    public float rotateSpeed;
-    public float rotateAccelate;
 
-    public float maxRotateSpeed;
-    public float minRotateSpeed;
-
-    void Update()
-    {
-        transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
-    }
-    
-    public void SpeedUp()
-    {
-        rotateSpeed += rotateAccelate * Time.deltaTime;
-
-        if (rotateSpeed > maxRotateSpeed)
-        {
-            rotateSpeed = maxRotateSpeed;
-        }
-    }
-    public void SpeedDown()
-    {
-        rotateSpeed -= rotateAccelate * Time.deltaTime;
-
-        if (rotateSpeed < minRotateSpeed)
-        {
-            rotateSpeed = minRotateSpeed;
-        }
-    }
 }
